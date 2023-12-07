68fa66e3d08f863a676008b2a080d878d3a73afc
wolfgames <36765@ma-web.nl>
2023-12-07 14:51:13 +0100

    goose

--- 01 C/app.js
+++ 01 C/app.js
@@ -15,14 +15,28 @@ class App {
         console.log("distributer:"+distributer)
         let darkmode=true
         console.log("darkmode:"+darkmode)
+        let boll=false
+        console.log("boll:"+boll)
+        let counter=100
+        console.log("counter:"+counter)
+        let words=aaaaaaaaaaaaa
+        console.log("words:"+words) 
+
 
 }           
 }
 
 let myApp = new App();
 myApp.runApplication(); 
-
-c
+console.log  ("appnaam:"+appnaam);
+console.log("versienumer:"+versienumer);
+console.log("versiedatum:"+versiedatum);
+console.log ("atheur:"+atheur);
+console.log("copyright:"+copyright);
+console.log("distributer:"+distributer);
+console.log("darkmode:"+darkmode);
+console.log("boll:"+boll);
+console.log("words:"+words); 
 
 
 
